5aef737c3eac372744ec987c0dcbce885ea0aac1
Kevin Harrington <mad.hephaestus@gmail.com>
2011-07-10 22:53:34 +0000

    removing extra files

--- application/tools/NRLauncher/WebServer/bin/ws.sh
+++
@@ -1,2 +0,0 @@
-# /bin/sh
-evm -cp /mnt/cf/ws/wsz.jar:/mnt/cf/ws/jsdk.jar Acme.Serve.Serve -a aliases.properties -p 8080 -l
--- application/tools/NRLauncher/WebServer/webroot/cgi/test.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Content-type: text/html
-echo.
-echo ^<html^>^<body^>^<h2^>Hello World!^</h2^>^<pre^>
-dir /b
-echo ^</pre^>^</body^>^</html^>
