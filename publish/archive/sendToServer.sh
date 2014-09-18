8f4e8e159456daf376ac0552fc144ba968c93197
madhephaestus <mad.hephaestus@gmail.com>
2014-09-18 12:48:25 -0400

    Cleaner server sending

--- publish/archive/sendToServer.sh
+++ publish/archive/sendToServer.sh
@@ -11,21 +11,8 @@ if ( test -z "$GH_TOKEN" ) then
 	exit 1
 fi
 if (! test -z "$1" ) then
-	#cp ../installer-scripts/windows/nrdk-$VERSION.exe $VERSION/
-	ruby ghupload.rb
 
-	#curl --form "fileupload=@$VERSION/nrdk-$VERSION.exe" https://uploads.github.com/repos/NeuronRobotics/java-bowler/releases/$VERSION/
-	
-	#Debian linux
-	#s3cmd put 				--acl-public --guess-mime-type $VERSION/nr-rdk-java_$VERSION-1_all.deb 	s3://downloads.bowler.io/nrdk/$VERSION/nr-rdk-java_$VERSION-1_all.deb
-	
-	#windows
-	#s3cmd put 				--acl-public --guess-mime-type $VERSION/nrdk-$VERSION.exe 				s3://downloads.bowler.io/nrdk/$VERSION/nrdk-$VERSION.exe
-	#s3cmd put 				--acl-public --guess-mime-type dyio-drivers-1.0.1.exe 					s3://downloads.bowler.io/drivers/dyio-drivers-1.0.1.exe
-	
-	#zip file
-	#s3cmd put 				--acl-public --guess-mime-type $VERSION/nrdk-$VERSION.zip 				s3://downloads.bowler.io/nrdk/$VERSION/nrdk-$VERSION.zip
-	
+	ruby ghupload.rb
 
 else
 	echo #####ERROR no version specified, I.E. 3.7.0
