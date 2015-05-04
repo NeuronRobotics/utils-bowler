04e0de680025c5c24d91e6c397cba7d999451bd5
Kevin harrington <mad.hephaestus@gmail.com>
2015-05-04 17:35:47 -0400

    adding the launcher script

--- publish/installer-scripts/osx/prep.sh
+++ publish/installer-scripts/osx/prep.sh
@@ -10,11 +10,13 @@ if (! test -z "$VERSION" ) then
 	unzip -qq nrdk-*.zip -d rdk
 	unzip -qq ~/git/ZipArchive/mac/opencv249build.zip -d rdk/
 	unzip -qq ~/git/ZipArchive/linux/Slic3r.app.zip -d rdk/
+	chmod +x BowlerStudio
+	cp BowlerStudio rdk/
 
 	mv rdk/nrdk-*/* rdk/
 	rm -rf rdk/nrdk-*
 	rm nrdk-*.zip
 	
-	zip -r mac-$VERSION rdk
+	zip -r mac-$VERSION.zip rdk
 
 fi
