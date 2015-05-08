29192ccd311ce954050204f4b5f54ad99aaf8180
hephaestus <mad.hephaestus@gmail.com>
2015-05-08 17:57:47 -0400

    correct settings to remove modemmanager

--- publish/installer-scripts/linux/build/makeDeb.sh
+++ publish/installer-scripts/linux/build/makeDeb.sh
@@ -1,5 +1,5 @@
 DEPENDS='openjdk-8-jdk,openjfx,libopencv2.4-java,libopencv2.4-jni'
 sudo apt-get install checkinstall
-sudo checkinstall --requires $DEPENDS --remove 'modemmanager' --arch all --pkglicense Apache  --maintainer "Customer Support '<info@neuronrobotics.com>'" --install=yes -y
+sudo checkinstall --requires $DEPENDS --conflicts 'modemmanager' --replaces 'modemmanager' --arch all --pkglicense Apache  --maintainer "Customer Support '<info@neuronrobotics.com>'" --install=yes -y
 
 
