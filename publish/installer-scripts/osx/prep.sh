0152b2531bfc6e1f2ba4c950c81b2847f0f52802
hephaestus <mad.hephaestus@gmail.com>
2015-06-23 12:32:28 -0400

    use the version name as the folder name

--- publish/installer-scripts/osx/prep.sh
+++ publish/installer-scripts/osx/prep.sh
@@ -2,22 +2,21 @@
 
 START=$PWD
 VERSION=$1
+DIR=bowlerstudio-$VERSION
 if (! test -z "$VERSION" ) then
-	rm -rf rdk
-	rm -rf bowlerstudio-$VERSION
+	rm -rf $DIR
 	
-	mkdir rdk
-	cp -r BowlerStudio.app rdk/
-	unzip -qq bowlerstudio-*.zip -d rdk/BowlerStudio.app/Contents/MacOS/
-	unzip -qq ~/git/ZipArchive/mac/opencv249build.zip -d rdk/BowlerStudio.app/Contents/MacOS/
-	unzip -qq ~/git/ZipArchive/mac/Slic3r.app.zip -d rdk/
-	chmod +x BowlerStudio
-	cp BowlerStudio rdk/BowlerStudio.app/Contents/MacOS/
+	mkdir $DIR
+	cp -r BowlerStudio.app $DIR/
+	unzip -qq bowlerstudio-*.zip -d $DIR/BowlerStudio.app/Contents/MacOS/
+	mv $DIR/BowlerStudio.app/Contents/MacOS/$DIR/* $DIR/BowlerStudio.app/Contents/MacOS/
 
-	rm -rf rdk/bowlerstudio-*
+	unzip -qq ~/git/ZipArchive/mac/opencv249build.zip -d $DIR/BowlerStudio.app/Contents/MacOS/
+	unzip -qq ~/git/ZipArchive/mac/Slic3r.app.zip -d $DIR/
+	chmod +x $DIR/BowlerStudio.app/Contents/MacOS/BowlerStudio
 	rm *.zip
 	
-	zip  -qq -r mac-$VERSION.zip rdk
-	rm -rf rdk/
+	zip  -qq -r mac-$VERSION.zip $DIR
+	rm -rf $DIR/
 
 fi
