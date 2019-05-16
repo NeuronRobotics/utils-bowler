000b9d635d0569c3359485427cb9b2826120eca9
Kevin Harrington <mad.hephaestus@gmail.com>
2019-05-16 01:37:07 -0400

    packed with a packager

--- publish/installer-scripts/osx/prep.sh
+++ publish/installer-scripts/osx/prep.sh
@@ -3,18 +3,41 @@
 START=$PWD
 VERSION=$1
 DIR=bowlerstudio-$VERSION
+JVMIMG="zulu8.38.0.13-ca-fx-jdk8.0.212-macosx_x64.tar.gz"
+JVMDIR="${JVMIMG%%.*}"
+echo "Dir = $JVMDIR"
 if (! test -z "$VERSION" ) then
 	rm -rf $DIR
 	
-	mkdir $DIR
-	cp -r BowlerStudio.app $DIR/
-	unzip -qq bowlerstudio-*.zip -d $DIR/BowlerStudio.app/Contents/MacOS/
-	mv $DIR/BowlerStudio.app/Contents/MacOS/$DIR/* $DIR/BowlerStudio.app/Contents/MacOS/
-
-	chmod +x $DIR/BowlerStudio.app/Contents/MacOS/BowlerStudio
-	rm *.zip
+	#cp -r BowlerStudio.app $DIR/
+	unzip -qq $PWD/../../archive/$VERSION/bowlerstudio-$VERSION.zip -d .
+	if [ -f "$JVMIMG" ]; then
+    	echo "$JVMIMG exist"
+    else
+    	wget https://cdn.azul.com/zulu/bin/$JVMIMG
+	fi
+	if [ -f jvm.zip ]; then
+    	echo "$JVMIMG exist"
+    else
+    	tar xzf $JVMIMG && zip jvm.zip $(tar tf $JVMIMG)
+    	rm -rf $JVMDIR*
+	fi
 	
-	zip  -qq -r mac-$VERSION.zip $DIR
+	java -jar packr.jar \
+     --platform mac \
+     --jdk jvm.zip \
+     --executable BowlerStudio \
+     --classpath $DIR/bin/BowlerStudio.jar \
+     --mainclass com.neuronrobotics.bowlerstudio.BowlerStudio \
+     --vmargs Xmx8G \
+     --icon $DIR/bin/NeuronRobotics.ico \
+     --output $DIR/BowlerStudio.app
+	#mv $DIR/BowlerStudio.app/Contents/MacOS/$DIR/* $DIR/BowlerStudio.app/Contents/MacOS/
+
+	#chmod +x $DIR/BowlerStudio.app/Contents/MacOS/BowlerStudio
+	#rm *.zip
+	
+	zip  -qq -r mac-$VERSION.zip $DIR/BowlerStudio.app/
 	rm -rf $DIR/
 
 fi
