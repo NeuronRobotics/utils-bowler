8f0336efd372210add250b787ff9884d2e5fd39c
Kevin Harrington <mad.hephaestus@gmail.com>
2019-05-16 16:06:06 -0400

    build 2 types of packages

--- publish/installer-scripts/osx/prep.sh
+++ publish/installer-scripts/osx/prep.sh
@@ -3,8 +3,9 @@
 START=$PWD
 VERSION=$1
 DIR=bowlerstudio-$VERSION
-JVMIMG="zulu8.38.0.13-ca-fx-jdk8.0.212-macosx_x64.tar.gz"
-JVMDIR="${JVMIMG%%.*}"
+JVMDIR=zulu8.38.0.13-ca-fx-jdk8.0.212-macosx_x64
+JVMIMG=$JVMDIR.tar.gz
+
 PACKER=$JAVA_HOME/../bin/javapackager
 $PACKER    -version
 JVM_CONTENT="jvmcontents"
@@ -24,27 +25,42 @@ if (! test -z "$VERSION" ) then
 	    	echo Downloading from https://www.azul.com/downloads/zulu/zulufx/ 
 	    	wget https://cdn.azul.com/zulu/bin/$JVMIMG
 		fi
-    	tar xzf $JVMIMG -C $JVM_CONTENT
-		zip jvm.zip $JVM_CONTENT
+		rm -rf $JVM_CONTENT
+		mkdir $JVM_CONTENT
+		echo "Untaring $JVMIMG"
+    	tar -xzf $JVMIMG 
+    	mv $JVMDIR/* $JVM_CONTENT
+    	rm -rf $JVMDIR/
+		zip -r jvm.zip $JVM_CONTENT/*
 	fi
 	
 	if [ ! -d "$JVM_CONTENT" ]; then
 	 unzip -qq jvm.zip -d $JVM_CONTENT
+	 else
+	 echo "Existing jvm : $JVM_CONTENT"
 	fi
-	
-	$PACKER    -deploy \
-			   -Bruntime=$JVM_CONTENT \
+	rm -rf $DIR/installer/
+	$PACKER    -deploy -Bruntime=$JVM_CONTENT \
 			   -native image \
 			   -name BowlerStudio \
 			   -BappVersion=$VERSION \
 			   -Bicon=icons.icns \
-			   -srcdir . \
+			   -Bmac.CFBundleVersion=1.0 \
+			   -Bmac.CFBundleIdentifier=com.neuronrobotics.bowlerstudio.BowlerStudio \
+			   -Bmac.category=Education \
+			   -BjvmOptions=-Xms8g \
+			   -BjvmOptions=-XX:+UseConcMarkSweepGC \
+			   -BjvmOptions=-Dapple.laf.useScreenMenuBar=true \
+			   -BjvmOptions=-Dcom.apple.smallTabs=true \
+			   -srcdir $DIR/ \
 			   -srcfiles $DIR/bin/BowlerStudio.jar \
 			   -appclass com.neuronrobotics.bowlerstudio.BowlerStudio \
 			   -outdir $DIR/installer/ \
 			   -v \
-			   -outfile BowlerStudio-$VERSION-Mac-Installer
-			   
+			   -nosign \
+			   -outfile BowlerStudio.app
+	echo "Second packer"
+	rm -rf BowlerStudio.app/
 	java -jar packr.jar \
      --platform mac \
      --jdk jvm.zip \
@@ -53,12 +69,12 @@ if (! test -z "$VERSION" ) then
      --mainclass com.neuronrobotics.bowlerstudio.BowlerStudio \
      --vmargs Xmx8G \
      --icon $DIR/bin/NeuronRobotics.ico \
-     --output $DIR/BowlerStudio.app\
+     --output BowlerStudio.app\
      --verbose
     echo "Zipping ..."
     rm -rf mac-installer-$VERSION.zip
 	zip  -qq -r mac-installer-$VERSION.zip $DIR/installer/
 	rm -rf mac-$VERSION.zip
-	zip  -qq -r mac-$VERSION.zip $DIR/BowlerStudio.app/
+	zip  -qq -r mac-$VERSION.zip BowlerStudio.app/
 	echo "Zipping Done!"
 fi
