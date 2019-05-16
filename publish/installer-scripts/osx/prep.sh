9b712da708537b330d62b883030f70abe7702880
Kevin Harrington <mad.hephaestus@gmail.com>
2019-05-16 15:20:36 -0400

    trying other packager

--- publish/installer-scripts/osx/prep.sh
+++ publish/installer-scripts/osx/prep.sh
@@ -5,7 +5,10 @@ VERSION=$1
 DIR=bowlerstudio-$VERSION
 JVMIMG="zulu8.38.0.13-ca-fx-jdk8.0.212-macosx_x64.tar.gz"
 JVMDIR="${JVMIMG%%.*}"
-echo "Dir = $JVMDIR"
+PACKER=$JAVA_HOME/../bin/javapackager
+$PACKER    -version
+JVM_CONTENT="jvmcontents"
+echo "Dir = $JVMDIR packaged by: $PACKER"
 if (! test -z "$VERSION" ) then
 	rm -rf $DIR
 	
@@ -21,10 +24,27 @@ if (! test -z "$VERSION" ) then
 	    	echo Downloading from https://www.azul.com/downloads/zulu/zulufx/ 
 	    	wget https://cdn.azul.com/zulu/bin/$JVMIMG
 		fi
-    	tar xzf $JVMIMG && zip jvm.zip $(tar tf $JVMIMG)
-    	rm -rf $JVMDIR*
+    	tar xzf $JVMIMG -C $JVM_CONTENT
+		zip jvm.zip $JVM_CONTENT
 	fi
 	
+	if [ ! -d "$JVM_CONTENT" ]; then
+	 unzip -qq jvm.zip -d $JVM_CONTENT
+	fi
+	
+	$PACKER    -deploy \
+			   -Bruntime=$JVM_CONTENT \
+			   -native image \
+			   -name BowlerStudio \
+			   -BappVersion=$VERSION \
+			   -Bicon=icons.icns \
+			   -srcdir . \
+			   -srcfiles $DIR/bin/BowlerStudio.jar \
+			   -appclass com.neuronrobotics.bowlerstudio.BowlerStudio \
+			   -outdir $DIR/installer/ \
+			   -v \
+			   -outfile BowlerStudio-$VERSION-Mac-Installer
+			   
 	java -jar packr.jar \
      --platform mac \
      --jdk jvm.zip \
@@ -35,7 +55,10 @@ if (! test -z "$VERSION" ) then
      --icon $DIR/bin/NeuronRobotics.ico \
      --output $DIR/BowlerStudio.app\
      --verbose
-	
+    echo "Zipping ..."
+    rm -rf mac-installer-$VERSION.zip
+	zip  -qq -r mac-installer-$VERSION.zip $DIR/installer/
+	rm -rf mac-$VERSION.zip
 	zip  -qq -r mac-$VERSION.zip $DIR/BowlerStudio.app/
-
+	echo "Zipping Done!"
 fi
